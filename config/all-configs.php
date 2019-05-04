3c3e83e3f76adc8a6659c2044f8ccb30a9b906e6
contact@luclaverdure.com <contact@luclaverdure.com>
2019-05-04 01:03:28 -0400

    Committing all config file

--- config/all-configs.php
+++ config/all-configs.php
@@ -11,12 +11,21 @@
 		public const ACTIVE_THEME = 'WizardHat';
 		
 		// define theme
-		public const CRON_JOB_TOKEN = '';
+		public const CRON_JOB_TOKEN = 'ddfc336faff4f45bb9beff4bea319364';
+		
+		static public $CMS_DB_HOST;
+		static public $CMS_DB_USER;
+		static public $CMS_DB_PASS;
+		static public $CMS_DB_NAME;
 
-		// CMS connection to DB
-		public const CMS_DB_HOST = '';
-		public const CMS_DB_USER = '';
-		public const CMS_DB_PASS = '';
-		public const CMS_DB_NAME = '';
+		public function __construct() {
+
+			include "cms-configs.php";
+
+			self::$CMS_DB_HOST = $CMS_DB_HOST;
+			self::$CMS_DB_USER = $CMS_DB_USER;
+			self::$CMS_DB_PASS = $CMS_DB_PASS;
+			self::$CMS_DB_NAME = $CMS_DB_NAME;
+		}
 	}
 ?>
